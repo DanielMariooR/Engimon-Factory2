27d0f8d961a9134fae0691448ad3ac9a55ca7859
DanielMariooR <13519031@std.stei.itb.ac.id>
2021-04-05 23:41:13 +0700

    add set and get engimon

--- Engimon.java
+++ Engimon.java
@@ -1,3 +1,4 @@
+import java.awt.Point;
 import java.util.*;
 
 class Engimon{
@@ -12,5 +13,43 @@ class Engimon{
     private ArrayList<String> parentSpecies;
     private ArrayList<Skill> skills;
     private Point position;
+
+    public void setLevel(int lvl){
+        this.level = lvl;
+    }
+
+    public void setExp(int xp){
+        this.exp = xp;
+    }
+
+    public String getSpecies(){
+        return this.species;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public List<Skill> getSkills(){
+        return this.skills;
+    }
+
+    public void insertSkill(Skill newSkill){
+        this.skills.add(newSkill);
+    }
+
+    public void move(int x, int y){
+        this.position.setX(position.getX()+x);
+        this.position.setY(position.getY()+y);
+    }
+
+    public void setPos(Position p){
+        this.position.setX(p.getX());
+        this.position.setY(p.getY());
+    }
+
+    public Position getPos(){
+        return this.position;
+    }
 
 }
