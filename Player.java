0ec81e06b65e4a5b1cff364ed4368ef04daf286f
DanielMariooR <13519031@std.stei.itb.ac.id>
2021-04-25 23:23:03 +0700

    fix canvas issue

--- Player.java
+++ Player.java
@@ -112,44 +112,6 @@ public class Player{
             }
         }
     }
-    // public void useSkillItem(){
-    //     Skill skill = temp.getSkill();
-
-    //     boolean found = false;
-    //     boolean cancel = false;
-
-    //     while(!found && !cancel){
-    //         showAllEngimon();
-    //         System.out.println("Enter the engimon to learn skill!\n");
-    //         System.out.println("Enter 0 to cancel!\n");
-
-    //         if(id == 0) cancel = true ;
-
-    //         else {
-    //             boolean elemFound = false;
-    //             Engimon current = ownedEngimon.getItem(id);
-    //             for(String engimonElem: current.getElem()){
-    //                 for(String skillElem: skill.getElem()){
-    //                     if(engimonElem.equals(skillElem) && !found){
-    //                         elemFound = true;
-    //                         if(current.getSkills().size() == 4){
-    //                             System.out.println("Your engimon cannot have more than 4 skills\n");
-    //                             current.showSkills();
-    //                             System.out.println("Choose skill to forget!\n");
-    //                             int skillid = scan.nextInt();
-    //                             current.removeSkill(skillid-1);
-    //                         }
-
-    //                         current.insertSkill(skill);
-    //                         found = true;
-    //                         break;
-    //                     } 
-    //                 }
-    //             }
-    //             if(!elemFound) System.out.println("The Engimon you choose are not compatible with the skill\n");
-    //         }
-    //     }
-    // }
 
     public void addSkillItem(Item item){
         
--- Resource.java
+++ Resource.java
@@ -1,6 +1,5 @@
 import java.util.*;
 
-import jdk.tools.jlink.builder.ImageBuilder;
 
 import java.awt.Image;
 import java.awt.Toolkit;
